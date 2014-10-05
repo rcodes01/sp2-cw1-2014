d3491b18b353aec11117049aaa51c73710605560
Ricardo Pedrosa <rpedro02@mail.bbk.ac.uk>
2014-10-05 23:51:44 +0100

    Test Git

---
+++ cw1/Coursework1.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of class Coursework1 here.
+ * 
+ * @author Ricardo Pedrosa 
+ * @version 1.0
+ */
+public class Coursework1
+{
+    
+    public static void main(String args[]) {
+        System.out.println("Test Git");
+    }
+}
