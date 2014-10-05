502160fb14f5b9b0bb0d894255f3dc2e92168046
Ricardo Pedrosa <rpedro02@mail.bbk.ac.uk>
2014-10-06 00:29:20 +0100

    displayCommon() method

--- Coursework1.java
+++ Coursework1.java
@@ -5,10 +5,22 @@
  * @author Ricardo Pedrosa 
  * @version 1.0
  */
-public class Coursework1
-{
+public class Coursework1 {
     
-    public static void main(String args[]) {
-        System.out.println("Test Git");
+    public static void displayCommon(int[] a, int[] b) {
+        String common = "";
+        for(int elem_a : a) {
+            for(int elem_b : b) {
+                if(elem_a == elem_b) common += elem_a + " ";
+            }
+        }
+        System.out.println("Common data is: " + common);
+    }
+    
+    
+    public static void main(String[] args) {
+        int[] set1 = {2, 5, 6, 8, 9};
+        int[] set2 = {3, 5, 7, 8, 10};
+        displayCommon(set1, set2);
     }
 }
